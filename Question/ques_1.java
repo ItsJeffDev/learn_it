c436b060b8290fc883bf8905ed0866d3b7e8003d
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-07-23 14:50:36 +0800

    3 question

---
+++ Question/ques_1.java
@@ -0,0 +1,60 @@
+public class ques_1 {
+    public static void main(String[] args) {
+        // Write a program that takes an array of integers as
+        // input and calculates the sum of all the elements in the array using a loop.
+
+        // int[] arr = {1, 2, 3, 4, 5};
+
+        // int sum = 0;
+
+        // for (int x : arr){
+        // sum += x;
+        // }
+        // System.out.println(sum);
+
+        // Output: 15
+
+        // Write a program that takes an array of integers as
+        // input and finds the maximum element in the array using a loop.
+
+        // int[] arr = { 10, 5, 20, 8, 15 };
+
+        // int max = arr[0];
+
+        // for (int i = 0; i < arr.length; i++) {
+        // if (arr[i] > max) {
+        // max = arr[i];
+        // }
+        // }
+        // System.out.println(max);
+
+        // Output: 20
+
+        // Write a program that takes a 2-dimensional array (matrix)
+        // of integers as input and prints all the elements of the array using nested
+        // loops.
+
+        // int[][] arr = {
+        //         { 1, 2, 3 },
+        //         { 4, 5, 6 },
+        //         { 7, 8, 9 }
+        // };
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int in = 0; in < arr[i].length; in++){
+        //         System.out.print(arr[i][in] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Output:  
+        // 1 2 3 
+        // 4 5 6 
+        // 7 8 9 
+
+        // Write a program that takes a 2-dimensional array (matrix) of integers as input and calculates the
+        // sum of elements in each row using nested loops. Print the sum for each row.
+
+        
+    }
+}
