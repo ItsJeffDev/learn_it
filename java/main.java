78ad5615fa1a31e5fbb61922e7fd7e5349a6d33d
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-07-23 14:19:54 +0800

    learn

--- java/main.java
+++ java/main.java
@@ -1,10 +1,14 @@
-package java;
-
 public class main {
     public static void main(String[] args) {
-        
-        character c = new character("David", "Hello", 100, 20, 15);
-
-        System.out.println(c);
+        int[][] nums = {{1,2,3,4,5,6,7,8,9,10}, {11,12,13,14,15,16,17,18,19,20}};
+        for (int[] x : nums){
+            for (int i : x){
+                if (i == 6){
+                    continue;
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
     }
 }
