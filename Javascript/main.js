016803614927ac63d888cf0f9323935878d5bf80
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-07-24 08:45:28 +0800

    learn

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,36 @@
-cons
+
+
+
+
+
+
+
+
+
+// let car = {
+//     brand: "BMW",
+//     model: "Sports",
+//     start: function(){
+//         console.log("Hello!");
+//     }
+// };
+
+// console.log(Object.values(car));
+// console.log(JSON.stringify(car));
+
+
+
+// class Person{
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     start() {
+//         console.log("The People Starting!");
+//     }
+// }
+
+// let Jack = new Person("Jack", 20);
+
+// console.log(JSON.stringify(Jack));
+
