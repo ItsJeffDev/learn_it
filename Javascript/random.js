e479ab3e6a60c3fa32922203743ee7599e1331e8
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-08-22 10:47:39 +0800

    learn

--- Javascript/random.js
+++ Javascript/random.js
@@ -1,13 +1,62 @@
-let train = {
-    from:"Legazpi City",
-    to:"Manila City",
-    price:3000
-};
+// let train = {
+//     from:"Legazpi City",
+//     to:"Manila City",
+//     price:3000
+// };
 
-console.log(`Ticket From: ${train.from}`);
-console.log(`Ticket To: ${train.to}`);
-console.log(`Ticket Price: ${train.price}`);
+// console.log(`Ticket From: ${train.from}`);
+// console.log(`Ticket To: ${train.to}`);
+// console.log(`Ticket Price: ${train.price}`);
+
+// let names = {};
+// names.name = "Jeff";
+// names.age = 17;
+// console.log(names);
+
+let books = [
+    {
+        title:"Speaking JavaScript",
+        author:"Axel Rauschmayer",
+        num_pages:460
+    },
+    {
+        title:"Programming JavaScript Applications",
+        author:"Eric Elliott",
+        num_pages:254        
+    },
+    {
+        title:"Understanding ECMAScript 6",
+        author:"Nicholas C. Zakas",
+        num_pages:352  
+    }
+];
+
+let new_books = {
+        title:"Learning JavaScript Design Patterns",
+        author:"Addy Osmani",
+        num_pages: 254
+}
+
+books.push(new_books);
+console.log(books[0].title);
+console.log(books[1].title);
+console.log(books[2].title);
+console.log(books[3].title);
 
 
+let copy = books.slice(2);
+
+let sum = "";
+
+for (let i = 0; i < books.length; i++){
+    sum += books[i].title
+}
+
+console.log(sum);
+
+
+let huh = ["jef","add","dqq"]
+console.log(huh.push("g"));
+console.log(huh);
 
 
---
+++ Question/exam.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class exam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Age: ");
+        // int age = sc.nextInt();
+
+        // if(age >= 18){
+        //     System.out.println("You are eligible to vote.");
+        // } else {
+        //     System.out.println("You are not eligible to vote.");
+        // }
+
+        // Write a Java program that asks the user to enter 5 integers, 
+        // then Print all the even numbers among them.
+
+        // int user;
+
+        // System.out.print("Enter 5 numbers: ");
+        // for (int i = 0; i < 5; i++){
+        //     user = sc.nextInt();
+
+        //     if (user % 2 == 0){
+        //         System.out.println(user + " is even.");
+        //     }
+        // }
+
+        //Asks the user to enter 5 integers, Stores them in an array, Then calculates and prints the sum of all the numbers.
+
+        int[] array = {};
+        
+        for()
+    }
+}
