6b8a5d48b51fa3b29eb69ec61da0b7a96573367c
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-08-22 08:57:53 +0800

    learn

--- Javascript/random.js
+++ Javascript/random.js
@@ -1,3 +1,13 @@
-let array = ["Hello", 15, 10];
+let train = {
+    from:"Legazpi City",
+    to:"Manila City",
+    price:3000
+};
 
-console.log(JSON.stringify(array));
+console.log(`Ticket From: ${train.from}`);
+console.log(`Ticket To: ${train.to}`);
+console.log(`Ticket Price: ${train.price}`);
+
+
+
+
