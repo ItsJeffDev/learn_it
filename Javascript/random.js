2ab4b8aa490878e5c5046861ec363240afaa4bc1
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-08-24 00:35:33 +0800

    cisco js

--- Javascript/random.js
+++ Javascript/random.js
@@ -19,22 +19,38 @@
 // console.log(8 + 2 + 5 * 2); // expected 39 
 
 
-let contacts = [{
-    name: "Maxwell Wright",
-    phone: "(0191) 719 6495",
-    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
-}, {
-    name: "Raja Villarreal",
-    phone: "0866 398 2895",
-    email: "posuere.vulputate@sed.com"
-}, {
-    name: "Helen Richards",
-    phone: "0800 1111",
-    email: "libero@convallis.edu"
-}];
+// let contacts = [{
+//     name: "Maxwell Wright",
+//     phone: "(0191) 719 6495",
+//     email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+// }, {
+//     name: "Raja Villarreal",
+//     phone: "0866 398 2895",
+//     email: "posuere.vulputate@sed.com"
+// }, {
+//     name: "Helen Richards",
+//     phone: "0800 1111",
+//     email: "libero@convallis.edu"
+// }];
 
+// let last = contacts.length - 1;
 
+// let last_name = window.prompt("Name: ");
+// let last_phone = window.prompt("Num: ");
+// let last_email = window.prompt("Email: ");
 
+// let new_contacts = {
+//     name: last_name,
+//     phone: last_phone,
+//     email: last_email
+// };
+
+// contacts.push(new_contacts);
+ 
+// console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+// console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+
+let last = "12"
 
 
 
